8470d9ea6f501c4e8da1e3df20a6f22cb62db1b2
Alin Popa <alin.popa@gmail.com>
2017-03-16 10:28:41 +0000

    Add more documentation to the existing code.

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,11 @@
+(**
+Some naming convensions here:
+  - `Example_lib` is the name provided in `src/jbuild`, under the `name` stanza;
+     this can be ignored if adding `(wrapped false)` in `src/jbuild` file.
+  - `Example01_lib` is made out from the name of the `src/example01_lib.ml` file name.
+  - `Example01` is the module name declared within the `src/example01_lib.ml` file.
+*)
+open Example_lib
 
 let () =
   print_endline ("Response: " ^ Example01_lib.Example01.thing)
